38841638164757a8bdc53b1dfd1396bf671baa1f
saimcan <saimcan0102@gmail.com>
2022-07-04 21:49:52 +0300

    Öğrenci Classı ikinci işlemler yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,10 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("merhaba dünya");
+
+
+        System.out.println("merhaba dünya 2");
+        System.out.println("merhaba dünya 3");
+
+
     }
 }
